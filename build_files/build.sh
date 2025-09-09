63deda1d9dd6e5cd4c11c6d5eb4fa92dee52ee75
forest reider <forest.reider@belimo.ch>
2025-09-09 09:13:09 +0200

    add some sddm theming and wallpapers

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,7 +8,7 @@ set -ouex pipefail
 dnf copr -y enable yalter/niri
 
 # install
-dnf install -y gcc micro nautilus niri openfortivpn
+dnf install -y gcc micro nautilus niri nu openfortivpn
 
 # remove
 dnf remove -y firefox
@@ -16,6 +16,12 @@ dnf remove -y firefox
 # clean
 dnf clean all
 
+# sddm theme
+nu -c 'http get https://api.github.com/repos/catppuccin/sddm/releases/latest | get $.assets.browser_download_url | where ($it | str ends-with "catppuccin-mocha-lavender-sddm.zip") | get 0 | http get $in | save -r "catppuccin.zip"'
+unzip -o catppuccin.zip -d /usr/share/sddm/themes
+rm catppuccin.zip
+curl -o /usr/share/wallpapers/cosmic.jpg https://raw.githubusercontent.com/tino376dev/dofiles/.config/wallpapers/cosmic.jpg
+curl -o /usr/share/wallpapers/cosmic-blur.jpg https://raw.githubusercontent.com/tino376dev/dofiles/.config/wallpapers/cosmic-blur.jpg
 # disable sway as session option
 # mv /usr/share/wayland-sessions/sway.desktop /usr/share/wayland-sessions/sway.desktop.disabled
 
