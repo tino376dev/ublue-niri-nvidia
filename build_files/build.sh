e3dbca3992defb0ed2de3399201b0e7170f51536
forest reider <forest.reider@belimo.ch>
2025-09-15 14:03:37 +0200

    turn on face icon in sddm

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ dnf copr -y enable yalter/niri
 dnf copr -y enable atim/starship
 
 # install
-dnf install -y bat fd-find gcc helix micro nautilus niri nu openfortivpn ripgrep starship zoxide
+dnf install -y bat fd-find fish gcc helix micro nautilus niri nu openfortivpn ripgrep starship zoxide
 
 # remove
 dnf remove -y firefox
@@ -22,6 +22,7 @@ nu -c 'http get https://api.github.com/repos/catppuccin/sddm/releases/latest | g
 unzip -o catppuccin.zip -d /usr/share/sddm/themes
 rm catppuccin.zip
 nu -c 'http get https://raw.github.com/tino376dev/files/main/cosmic-blur.png | save -rf /usr/share/sddm/themes/catppuccin-mocha-lavender/backgrounds/wall.png'
+nu -c 'open -r /usr/share/sddm/themes/catppuccin-mocha-lavender/theme.conf | str replace ''UserIcon="false"'' ''UserIcon="true"'' | save -rf /usr/share/sddm/themes/catppuccin-mocha-lavender/theme.conf'
 
 # flatpaks
 /usr/bin/flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
