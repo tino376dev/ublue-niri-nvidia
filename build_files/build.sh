41bbd47f002133421c639fc7440920c1717d42dd
forest reider <forest.reider@belimo.ch>
2025-11-18 08:10:20 +0100

    use png instead of jxl wallpapers

--- build_files/build.sh
+++ build_files/build.sh
@@ -46,10 +46,10 @@ rm -rf nu-*-x86_64-unknown-linux-gnu.tar.gz nu-*
 assets=$(curl -s https://api.github.com/repos/tino376dev/niri-extra-binaries/releases/latest | grep browser_download_url)
 echo "$assets" | grep awww-linux-amd64 | cut -d '"' -f 4 | xargs curl -Lo /usr/bin/awww
 echo "$assets" | grep awww-daemon-linux-amd64 | cut -d '"' -f 4 | xargs curl -Lo /usr/bin/awww-daemon
-echo "$assets" | grep light.jxl | cut -d '"' -f 4 | xargs curl -Lo /usr/share/backgrounds/light.jxl
-echo "$assets" | grep light-blur.jxl | cut -d '"' -f 4 | xargs curl -Lo /usr/share/backgrounds/light-blur.jxl
-echo "$assets" | grep dark.jxl | cut -d '"' -f 4 | xargs curl -Lo /usr/share/backgrounds/dark.jxl
-echo "$assets" | grep dark-blur.jxl | cut -d '"' -f 4 | xargs curl -Lo /usr/share/backgrounds/dark-blur.jxl
+echo "$assets" | grep light.png | cut -d '"' -f 4 | xargs curl -Lo /usr/share/backgrounds/light.png
+echo "$assets" | grep light-blur.png | cut -d '"' -f 4 | xargs curl -Lo /usr/share/backgrounds/light-blur.png
+echo "$assets" | grep dark.png | cut -d '"' -f 4 | xargs curl -Lo /usr/share/backgrounds/dark.png
+echo "$assets" | grep dark-blur.png | cut -d '"' -f 4 | xargs curl -Lo /usr/share/backgrounds/dark-blur.png
 chmod +x /usr/bin/awww /usr/bin/awww-daemon
 
 # sddm theme
