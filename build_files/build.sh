1974cf4dfcc0965e1e217e521596f0cf0d75e94e
forest reider <forest.reider@belimo.ch>
2025-11-19 15:57:55 +0100

    use niri-candy repo (old will break)

--- build_files/build.sh
+++ build_files/build.sh
@@ -43,14 +43,9 @@ rm -rf nu-*-x86_64-unknown-linux-gnu.tar.gz nu-*
 
 
 # get some extra binaries
-assets=$(curl -s https://api.github.com/repos/tino376dev/niri-extra-binaries/releases/latest | grep browser_download_url)
-echo "$assets" | grep awww-linux-amd64 | cut -d '"' -f 4 | xargs curl -Lo /usr/bin/awww
-echo "$assets" | grep awww-daemon-linux-amd64 | cut -d '"' -f 4 | xargs curl -Lo /usr/bin/awww-daemon
-echo "$assets" | grep light.png | cut -d '"' -f 4 | xargs curl -Lo /usr/share/backgrounds/light.png
-echo "$assets" | grep light-blur.png | cut -d '"' -f 4 | xargs curl -Lo /usr/share/backgrounds/light-blur.png
-echo "$assets" | grep dark.png | cut -d '"' -f 4 | xargs curl -Lo /usr/share/backgrounds/dark.png
-echo "$assets" | grep dark-blur.png | cut -d '"' -f 4 | xargs curl -Lo /usr/share/backgrounds/dark-blur.png
-chmod +x /usr/bin/awww /usr/bin/awww-daemon
+assets=$(curl -s https://api.github.com/repos/tino376dev/niri-candy/releases/latest | grep browser_download_url)
+curl -sSL $(echo "$assets" | grep awww-linux-amd64.tar.gz | cut -d '"' -f 4) | tar -xz -C /usr/bin
+curl -sSL $(echo "$assets" | grep wallpaper.tar.gz | cut -d '"' -f 4) | tar -xz -C /usr/share/backgrounds
 
 # sddm theme
 # mkdir /usr/share/sddm/themes/tino376dev
