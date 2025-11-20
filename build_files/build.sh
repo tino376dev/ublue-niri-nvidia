3f3f57d1bd66682b49b1e2a03172c97863d7bd01
forest reider <forest.reider@belimo.ch>
2025-11-20 14:42:34 +0100

    remove antigravity and choromxxxx

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,29 +10,13 @@ dnf copr -y enable lihaohong/yazi
 dnf copr -y enable scottames/ghostty
 dnf copr -y enable yalter/niri
 
-# google antigravity editor and chrome browser
-cat <<'EOF' | tee /etc/yum.repos.d/antigravity.repo
-[antigravity-rpm]
-name=Antigravity RPM Repository
-baseurl=https://us-central1-yum.pkg.dev/projects/antigravity-auto-updater-dev/antigravity-rpm
-enabled=1
-gpgcheck=0
-EOF
-
-dnf makecache
-
-dnf config-manager setopt google-chrome.enabled=1
-mkdir /var/opt
-
 # install
 dnf install -y \
-  antigravity \
   bat \
   du-dust \
   fd-find \
   fish \
   gcc \
-  google-chrome-stable \
   helix \
   git-delta \
   ghostty \
@@ -59,9 +43,9 @@ rm -rf nu-*-x86_64-unknown-linux-gnu.tar.gz nu-*
 
 
 # get some extra binaries
-# assets=$(curl -s https://api.github.com/repos/tino376dev/niri-candy/releases/latest | grep browser_download_url)
-# curl -sSL $(echo "$assets" | grep awww-linux-amd64.tar.gz | cut -d '"' -f 4) | tar -xz -C /usr/bin
-# curl -sSL $(echo "$assets" | grep wallpaper.tar.gz | cut -d '"' -f 4) | tar -xz -C /usr/share/backgrounds
+assets=$(curl -s https://api.github.com/repos/tino376dev/niri-candy/releases/latest | grep browser_download_url)
+curl -sSL $(echo "$assets" | grep awww-linux-amd64.tar.gz | cut -d '"' -f 4) | tar -xz -C /usr/bin
+curl -sSL $(echo "$assets" | grep wallpaper.tar.gz | cut -d '"' -f 4) | tar -xz -C /usr/share/backgrounds
 
 # sddm theme
 # mkdir /usr/share/sddm/themes/tino376dev
